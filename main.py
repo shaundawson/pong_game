d7abd8542c6c47b23ad91c1ecc493aa09ecb5c62
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-19 17:00:47 -0400

    set up the main screen

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from turtle import Screen
+
+
 # TODO: Set up the main screen
+screen = Screen()
+screen.setup(width=600, height=800)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+screen.exitonclick()
+
+
 # TODO: Create paddle that responds to key presses
 # TODO: Create another paddle
 # TODO: Create the ball and make it move
