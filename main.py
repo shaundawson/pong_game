7a676154e8c06635f5b4b896fa9e83f6488cdd46
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-19 17:01:19 -0400

    modify screen size

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import Screen
 
 # TODO: Set up the main screen
 screen = Screen()
-screen.setup(width=600, height=800)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
